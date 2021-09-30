35e5c073fd95b2eb8edabe678266da9841a11321
Caue97 <felipecauelage@gmail.com>
2021-09-30 12:53:05 -0300

    felcaue- - Começo do repositório e definição básica dos arquivos

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: felcaue- <felcaue-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/30 12:32:59 by felcaue-          #+#    #+#             */
+/*   Updated: 2021/09/30 12:46:14 by felcaue-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include "get_next_line.h"
+
+char	*get_next_line(int fd);
+
+#endif
